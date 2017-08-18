63491efbafe3bb45d9aef0689d9b02bb58d6ff3f
mcguigro <mcguigro@oregonstate.edu>
2017-08-18 13:45:27 -0700

    Main Menu Resolution Fix

---
+++ Assets/Scripts/UIScaler.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIScaler : MonoBehaviour {
+
+	private RectTransform trans;
+	public float scaleFactor;
+
+	// Use this for initialization
+	void Start () {
+		trans = GetComponent<RectTransform> ();
+		int height = Screen.height;
+		float scaling = ((float)Screen.height) / scaleFactor;
+		trans.localScale = new Vector3 (scaling, scaling, scaling);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
